bb9ca993d9c8a45af593b68a323a26c6aa524b2d
Nikhilkale2 <nikhilkale2105@gmail.com>
2026-01-06 13:25:07 +0530

    Practice locomotive scroll effect again

--- script.js
+++ script.js
@@ -15,6 +15,9 @@ gsap.from(".page2 #box", {
   scrollTrigger: {
     trigger: ".page2 #box",
     scroller: "body",
+    start: "top 60%",
+    end: "top 25%",
+    scrub: true,
   },
 });
 
@@ -27,6 +30,9 @@ gsap.from(".page3 #box", {
   scrollTrigger: {
     trigger: ".page3 #box",
     scroller: "body",
+    scrub: true,
+    start: "top 80%",
+    end: "top 30%",
   },
 });
 
@@ -36,4 +42,61 @@ gsap.from(".page4 #box", {
   rotate: 360,
   backgroundColor: "red",
   duration: 1.2,
+  scrollTrigger: {
+    trigger: ".page4 #box",
+    scroller: "body",
+    start: "top 70%",
+    end: "top 30%",
+    scrub: true,
+  },
+});
+
+gsap.from(".page5 img", {
+  opacity: 0,
+  scale: 0,
+  duration: 1.4,
+  scrollTrigger: {
+    trigger: ".page5 img",
+    scroller: "body",
+    start: "top 70%",
+    end: "top 40%",
+    scrub: true,
+  },
+});
+
+gsap.from(".page6 img", {
+  opacity: 0,
+  scale: 0,
+  duration: 1.4,
+  scrollTrigger: {
+    trigger: ".page6 img",
+    scroller: "body",
+    start: "top 70%",
+    end: "top 40%",
+    scrub: true,
+  },
+});
+gsap.from(".page7 img", {
+  opacity: 0,
+  scale: 0,
+  duration: 1.4,
+  scrollTrigger: {
+    trigger: ".page7 img",
+    scroller: "body",
+    start: "top 70%",
+    end: "top 40%",
+    scrub: true,
+  },
+});
+gsap.from(".page8 img", {
+  opacity: 0,
+  scale: 0,
+  duration: 1.4,
+  scrollTrigger: {
+    trigger: ".page8 img",
+    scroller: "body",
+    start: "top 70%",
+    end: "top 40%",
+    scrub: true,
+  },
 });
