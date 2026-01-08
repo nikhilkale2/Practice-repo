8bbc37f4ab4c3b6c00a6989e14550ffa1ffb7595
Nikhilkale2 <nikhilkaledev@gmail.com>
2026-01-08 13:10:42 +0530

    Add animation on different screen & make responsive

--- nav.js
+++ nav.js
@@ -1,33 +1,69 @@
-let el = gsap.timeline();
+const mm = gsap.matchMedia();
 
-el.to(".links", {
-  left: 0,
-  duration: 0.5,
-});
-el.from("li", {
-  x: -70,
-  duration: 0.3,
-  stagger: 0.2,
-  opacity: 0,
-});
-el.from(".icons i", {
-  x: -60,
-  duration: 0.3,
-  stagger: 0.3,
-  opacity: 0,
-});
-el.from("#close i", {
-  x: -60,
-  duration: 0.9,
-  opacity: 0,
+mm.add("(max-width: 768px)", () => {
+  let el = gsap.timeline({ paused: true });
+
+  el.to(".links", {
+    left: 0,
+    duration: 0.5,
+  });
+
+  el.from(".links li", {
+    x: -70,
+    opacity: 0,
+    duration: 0.3,
+    stagger: 0.2,
+  });
+
+  el.from(".icons i", {
+    x: -60,
+    opacity: 0,
+    duration: 0.3,
+    stagger: 0.3,
+  });
+
+  el.from("#close i", {
+    x: -60,
+    opacity: 0,
+    duration: 0.5,
+  });
+
+  document.querySelector("#menu").addEventListener("click", () => {
+    el.play();
+  });
+
+  document.querySelector("#close i").addEventListener("click", () => {
+    el.reverse();
+  });
+
+  // cleanup (important)
+  return () => {
+    el.kill();
+  };
 });
 
-el.pause();
-
-document.querySelector("#menu").addEventListener("click", function () {
-  el.play();
+mm.add("(min-width: 769px) and (max-width: 1024px)", () => {
+  let el = gsap.timeline();
+  el.from(".links li", {
+    opacity: 0,
+    duration: 0.6,
+    stagger: 0.3,
+    y: -150,
+  });
+  el.from(".icons i", {
+    y: -100,
+    opacity: 0,
+    duration: 0.6,
+    stagger: 0.5,
+  });
 });
 
-document.querySelector("#close i").addEventListener("click", function () {
-  el.reverse();
+mm.add("(min-width:1024px)", () => {
+  let el = gsap.timeline();
+  el.from(".links li", {
+    x: 150,
+    opacity: 0,
+    stagger: 0.25,
+    duration: 0.5,
+  });
 });
